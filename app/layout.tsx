0897425870a80ccea9facb90a69cd70c4a7c2b66
Cennet Gündoğdu <cennetgundogdu9920@gmail.com>
2023-10-20 15:27:49 +0300

    added all the sections and readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Create Next App',
+  title: 'Travel App',
   description: 'Travel UI/UX App for Camping',
 }
 
